6756d621ef0560d9f1bf81aadab1bdaa74d92827
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-02-28 15:27:48 -0300

    Updates

---
+++ precache-manifest.0c3a9f4644e0d93c2245bfa4ebda49aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7714ec2a0508da1a7f988c09fe116e26",
+    "url": "/MovieLibrary/index.html"
+  },
+  {
+    "revision": "fa03b5ad03c9dfc8dc16",
+    "url": "/MovieLibrary/static/css/main.74660e10.chunk.css"
+  },
+  {
+    "revision": "061f7d8e2fbfdbda99c5",
+    "url": "/MovieLibrary/static/js/2.05bf4c3c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/MovieLibrary/static/js/2.05bf4c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa03b5ad03c9dfc8dc16",
+    "url": "/MovieLibrary/static/js/main.13fdd9cb.chunk.js"
+  },
+  {
+    "revision": "a8f6a143ccb4d80d6464",
+    "url": "/MovieLibrary/static/js/runtime-main.a713c3fd.js"
+  },
+  {
+    "revision": "a551e8025148882dd69aa6a166e44ca3",
+    "url": "/MovieLibrary/static/media/search.a551e802.svg"
+  }
+]);
