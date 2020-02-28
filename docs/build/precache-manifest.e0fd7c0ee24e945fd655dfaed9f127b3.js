fbf71c81f8e9d0eb831a8e2b3a492f5146fd9a9c
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-02-28 15:18:54 -0300

    Adding docs

---
+++ docs/build/precache-manifest.e0fd7c0ee24e945fd655dfaed9f127b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d7462ae4a58fb2074d94d4041e8694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "274d6c419db7a5c6cf0d",
+    "url": "/static/css/main.74660e10.chunk.css"
+  },
+  {
+    "revision": "061f7d8e2fbfdbda99c5",
+    "url": "/static/js/2.05bf4c3c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.05bf4c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274d6c419db7a5c6cf0d",
+    "url": "/static/js/main.2f3692b2.chunk.js"
+  },
+  {
+    "revision": "a744514c683378e08c72",
+    "url": "/static/js/runtime-main.4deddc45.js"
+  },
+  {
+    "revision": "a551e8025148882dd69aa6a166e44ca3",
+    "url": "/static/media/search.a551e802.svg"
+  }
+]);
