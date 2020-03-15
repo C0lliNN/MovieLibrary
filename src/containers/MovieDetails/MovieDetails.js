34587afb52b74e020a0b3745b4e158ae750ec60f
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-03-15 10:30:14 -0300

    Converting Class-based Components to Funcional ones using Reack Hooks

--- src/containers/MovieDetails/MovieDetails.js
+++ src/containers/MovieDetails/MovieDetails.js
@@ -1,64 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Spinner from '../../components/Spinner/Spinner';
 import axios from 'axios';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import MovieInformation from '../../components/MovieInformation/MovieInformation'
 import {injectIntl} from 'react-intl'
 
-class MovieDetails extends Component {
+const MovieDetails = props => {
 
-    state = {
-        loading: true,
+    const [ requestState, setRequestState ] = useState({
+        isLoading: true,
         movie: null,
         error: null
-    }
+    })
 
-    render() {
-
-        let content = null;
-
-        if (this.state.loading) {
-            content = <Spinner text="Loading Movie Information..."/>
-        }
-
-        if (this.state.movie) {
-            content = <MovieInformation movie={this.state.movie} />
-        }
-
-        if (this.state.error) {
-            content = <ErrorMessage/>
-        }
-
-        return (
-            <section>
-                {content}
-            </section>
-        )
-    }
-
-    componentDidMount () {
-
-        const queryLanguage = this.props.intl.formatMessage({
+    useEffect(() => {
+        const queryLanguage = props.intl.formatMessage({
             id: 'languageAPIIdentifider',
             defaultMessage: 'en-US'
         })
 
-        axios.get(`/movie/${this.props.match.params.id }?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}`)
+        axios.get(`/movie/${props.match.params.id }?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}`)
         .then(response => {
-            this.setState({
-                loading: false,
+            setRequestState({
+                isLoading: false,
                 movie: response.data,
                 error: null
             })
         })
         .catch(error => {
-            this.setState({
-                loading: false,
+            setRequestState({
+                isLoading: false,
                 movie: null,
                 error: error
-            })
+            }) 
         })
+    })
+
+    let content = null;
+
+    if (requestState.isLoading) {
+        content = <Spinner text="Loading Movie Information..."/>
     }
+
+    if (requestState.movie) {
+        content = <MovieInformation movie={requestState.movie} />
+    }
+
+    if (requestState.error) {
+        content = <ErrorMessage/>
+    }
+
+    return (
+        <section>
+            {content}
+        </section>
+    )
+    
 
 }
 
--- src/containers/MovieSearch/MovieSearch.js
+++ src/containers/MovieSearch/MovieSearch.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import Spinner from '../../components/Spinner/Spinner'
 import Movies from '../../components/Movies/Movies';
@@ -6,144 +6,132 @@ import axios from 'axios'
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import {injectIntl} from 'react-intl'
 
-class MovieSearch extends Component {
+const MovieSearch = props => {
 
-    state = {
+    console.log(props)
+
+    const [ requestState, setRequestState ] = useState({
         movies: null,
-        loading: true,
-        error: false,
-        searchFieldValue: '',
-        queryString: ''
+        isLoading: true,
+        error: false
+    })
+
+    const [ searchFieldValue, setSearchFieldValue ] = useState('')
+    const [ queryString, setQueryString ] = useState('')
+
+    
+    const onValueChangeHandler = (event) => {
+        setSearchFieldValue(event.target.value)
     }
 
-    onValueChangeHandler = (event) => {
-        this.setState({
-            searchFieldValue: event.target.value
-        })
-    }
-
-    onSearchSubmitHandler = (event) => {
+    const onSearchSubmitHandler = (event) => {
         event.preventDefault();
 
-        if (this.state.searchFieldValue.trim() === '') {
+        if (searchFieldValue.trim() === '') {
             
-            this.props.history.push('/');
+            props.history.push('/');
 
         } else {
 
-            this.setState({
-                loading: true,
+            setRequestState({
+                isLoading: true,
                 error: null,
                 movies: null,
             })
 
-            this.props.history.push('?query=' + encodeURI(this.state.searchFieldValue));
+            props.history.push('?query=' + encodeURI(this.state.searchFieldValue));
        
         }
     }
 
-    render() {
+    const { intl } = props
 
-        let content = null; 
+    useEffect(() =>  {
 
-        if (this.state.loading) {
-            content = <Spinner text="Loading Most Popular Movies"/>
-        }
-
-        if (this.state.movies) {
-            content = <Movies 
-                movies={this.state.movies} 
-                query={this.state.queryString}
-                intl={this.props.intl}/>
-        }
-
-        if (this.state.error) {
-            content = <ErrorMessage message={this.state.error.message}/>
-        }
-
-        return (
-            
-            <section>
-                <SearchBar 
-                    value={this.state.searchFieldValue}
-                    onValueChange={this.onValueChangeHandler}
-                    onSearchSubmit={this.onSearchSubmitHandler}
-                    intl={this.props.intl}
-                />
-                {content}
-            </section>
-        )
-    }
-
-
-    componentDidMount() {
-
-        const queryLanguage = this.props.intl.formatMessage({
+        const queryLanguage = intl.formatMessage({
             id: 'languageAPIIdentifider',
             defaultMessage: 'en-US'
         })
 
         axios.get(`/movie/popular?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}&page=1`).then(response => {
 
-          this.setState({
+          setRequestState({
             movies: response.data.results,
-            loading: false,
+            isLoading: false,
             error: null
           })
 
         })
         .catch(error => {
-            this.setState({
+            setRequestState({
                 error: error,
                 movies: null,
                 loading: false
             })
         })
-    }
+    }, [intl])
 
-    static getDerivedStateFromProps(props, state) {
+    const { location } = props
+
+    useEffect(() => {
+        
+        const queryLanguage = intl.formatMessage({
+            id: 'languageAPIIdentifider',
+            defaultMessage: 'en-US'
+        })
+
+        const url = location.search ? `/search/movie?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}&page=1&include_adult=true&query=${location.search}` : `/movie/popular?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}&page=1`;
+
+        axios.get(url).then(response => {
+            setRequestState({
+                movies: response.data.results,
+                loading: false,
+                error: null
+            })
     
-        const queryString = new URLSearchParams(props.location.search).get('query')
-
-        if (state.queryString !== queryString) {
-            return {
-                queryString: queryString
-            }
-        }
-
-        return null;
-
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.queryString !== this.state.queryString) {
-
-            const queryLanguage = this.props.intl.formatMessage({
-                id: 'languageAPIIdentifider',
-                defaultMessage: 'en-US'
+        })
+        .catch(error => {
+            setRequestState({
+                error: error,
+                movies: null,
+                loading: false
             })
+        })
 
-            const url = this.state.queryString ? `/search/movie?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}&page=1&include_adult=true&query=${this.state.queryString}` : `/movie/popular?api_key=466eefcef086aaa1375e8ecfebc6a345&language=${queryLanguage}&page=1`;
-
-            axios.get(url).then(response => {
-                this.setState({
-                  movies: response.data.results,
-                  loading: false,
-                  error: null
-                })
-      
-            })
-            .catch(error => {
-                this.setState({
-                    error: error,
-                    movies: null,
-                    loading: false
-                })
-            })
-        }
-    }
-
+        setQueryString(location.search)
+        
+    }, [location, intl])
     
+
+    let content = null; 
+
+    if (requestState.isLoading) {
+        content = <Spinner text="Loading Most Popular Movies"/>
+    }
+
+    if (requestState.movies) {
+        content = <Movies 
+            movies={requestState.movies} 
+            query={queryString}
+            intl={props.intl}/>
+    }
+
+    if (requestState.error) {
+        content = <ErrorMessage message={requestState.error.message}/>
+    }
+
+    return (
+        
+        <section>
+            <SearchBar 
+                value={searchFieldValue}
+                onValueChange={onValueChangeHandler}
+                onSearchSubmit={onSearchSubmitHandler}
+                intl={props.intl}
+            />
+            {content}
+        </section>
+    )    
 
 }
 
